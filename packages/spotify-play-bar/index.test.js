efb08193fbf2a13c7a335ce5b8ae45a1243e6278
Hugh Rawlinson <hughr2005@gmail.com>
2019-05-31 10:13:38 -0400

    test: Change to new @testing-library/react package
    the react-testing-library dependency moved to @testing-library/react, so this commit pulls in the
    new package

--- packages/spotify-play-bar/index.test.js
+++ packages/spotify-play-bar/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, cleanup} from 'react-testing-library';
+import {render, cleanup} from '@testing-library/react';
 import SpotifyPlayBar from './index.js';
 import '@babel/polyfill'
 // More expect matchers are available with this, but I don't need them yet:
